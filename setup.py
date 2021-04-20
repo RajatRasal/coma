8125e83f15b335452c8acb739b005c1784fa01c3
Rajat Rasal <rrr2417@ic.ac.uk>
2021-04-20 17:53:15 +0100

    Packaging with pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='CoMA-UKBB-mesh',
+    version='0.1',
+    description='Implementation of CoMA for UKBB meshes dataset',
+    url='https://github.com/RajatRasal/coma_ukbiobank_mesh',
+    author='Rajat Rasal',
+    author_email='yugiohrajat1@gmail.com',
+    license='MIT',
+    packages=['coma'],
+    zip_safe=False
+)
