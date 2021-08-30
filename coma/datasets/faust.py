7ad931b573c7f09e898a69487744cf50a8f0f656
Rajat Rasal <rrr2417@ic.ac.uk>
2021-08-30 17:41:25 +0100

    FAUST all dataset and dataloader

--- coma/datasets/faust.py
+++ coma/datasets/faust.py
@@ -1,15 +1,16 @@
-from typing import Optional, Callable, List
-
+import random
 import shutil
 import os.path as osp
+from typing import Optional, Callable, List, Tuple
 
 import torch
+from torch_geometric.datasets import FAUST
 from torch.utils.data import DataLoader
 from torch_geometric.data import InMemoryDataset, Data, extract_zip
 from torch_geometric.io import read_ply
 
 
-class FAUST(InMemoryDataset):
+class FullFAUST(InMemoryDataset):
     """
     Adapted from Pytorch Geometric FAUST dataloader
     https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/datasets/faust.html#FAUST
@@ -22,8 +23,8 @@ class FAUST(InMemoryDataset):
                  pre_transform: Optional[Callable] = None,
                  pre_filter: Optional[Callable] = None):
         super().__init__(root, transform, pre_transform, pre_filter)
-        path = self.processed_paths[0] if train else self.processed_paths[1]
-        self.data, self.slices = torch.load(path)
+        # path = self.processed_paths[0] if train else self.processed_paths[1]
+        self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property
     def raw_file_names(self) -> str:
@@ -31,7 +32,7 @@ class FAUST(InMemoryDataset):
 
     @property
     def processed_file_names(self) -> List[str]:
-        return ['training.pt', 'test.pt']
+        return ['all_faust.pt']
 
     def download(self):
         raise RuntimeError(
@@ -43,23 +44,36 @@ class FAUST(InMemoryDataset):
 
         path = osp.join(self.raw_dir, 'MPI-FAUST', 'training', 'registrations')
         path = osp.join(path, 'tr_reg_{0:03d}.ply')
+
         data_list = []
+
         for i in range(100):
             data = read_ply(path.format(i))
-            data.y = torch.tensor([i % 10], dtype=torch.long)
+            data.person = torch.tensor([i % 10], dtype=torch.long)
+            data.pose = torch.tensor([i // 10], dtype=torch.long)
             if self.pre_filter is not None and not self.pre_filter(data):
                 continue
             if self.pre_transform is not None:
                 data = self.pre_transform(data)
             data_list.append(data)
 
-        torch.save(self.collate(data_list[:80]), self.processed_paths[0])
-        torch.save(self.collate(data_list[80:]), self.processed_paths[1])
+        torch.save(self.collate(data_list), self.processed_paths[0])
         shutil.rmtree(osp.join(self.raw_dir, 'MPI-FAUST'))
+
+def split_faust_by_person(dataset: FullFAUST, test_people_ids: List[int]) -> Tuple[List[Data], List[Data]]:
+    train = []
+    test = []
+    for data in dataset:
+        if data.person in test_people_ids:
+            test.append(data)
+        else:
+            train.append(data)
+    return train, test
+
 
 # TODO: Refactor BatchWrapper
 from collections import namedtuple
-BatchWrapper = namedtuple('BatchWrapper', ['x', 'features'])
+BatchWrapper = namedtuple('BatchWrapper', ['x', 'pose'])
 
 class FAUSTDataLoader(DataLoader):
 
@@ -68,7 +82,9 @@ class FAUSTDataLoader(DataLoader):
         def collate_fn(data_list: List[Data]):
             batch = torch.vstack([data.pos for data in data_list])
             batch = batch.reshape(-1, *data_list[0].pos.shape).double()
-            return BatchWrapper(x=batch, features=[])
+            pose = torch.vstack([data.pose for data in data_list])
+            pose = pose.reshape(-1, *data_list[0].pose.shape).double()
+            return BatchWrapper(x=batch, pose=pose)
 
         super(FAUSTDataLoader, self).__init__(
             dataset,
